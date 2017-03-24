b755939b567d69f45c4035d73db9a6c1ca2773ab
emRae <emRae@users.noreply.github.com>
2017-03-24 15:34:26 -0600

    stubbing out the boards component for realz

---
+++ client/components/Boards.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Boards extends Component {
+  state= { boards: []};
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+    return(
+      <div>
+        <h1>Hi! I'm the boards</h1>
+      </div>
+    )
+    
+  }
+}
+
+export default Boards;
